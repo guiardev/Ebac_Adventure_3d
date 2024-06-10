30fb18b5b8355e425e5ee6aa11b38a4c00937b1d
Guilherme A. R <guiabucarma@gmail.com>
2024-06-10 18:05:24 -0400

    add script GunBase

---
+++ Assets/Scripts/Gun/GunBase.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBase : MonoBehaviour{
+
+    private Coroutine _currentCoroutine;
+    public ProjectileBase prefabProjectile;
+
+    public Transform positionToShoot;
+    //public KeyCode keyCode = KeyCode.S;
+    public float timeBetweenShoot = .3f, speed = 50f;
+
+    /*
+    // Update is called once per frame
+    void Update(){
+
+        if(Input.GetKeyDown(keyCode)){
+            
+        }else if(Input.GetKeyUp(keyCode)){
+
+            
+        }
+    }
+    */
+
+    protected virtual IEnumerator ShootCoroutine(){
+
+        //esperar um tempo em cada tiro
+        while (true){
+            Shoot();
+            yield return new WaitForSeconds(timeBetweenShoot);
+        }
+    }
+
+    public virtual void Shoot(){
+
+        var projectile = Instantiate(prefabProjectile);
+        projectile.transform.position = positionToShoot.position;
+        projectile.transform.rotation = positionToShoot.rotation; // fazendo tiro atira centro player e seguir rotação do player
+
+        projectile.speed = speed;
+    }
+
+    public void StartShoot(){
+        StopShoot();
+        _currentCoroutine = StartCoroutine(ShootCoroutine());
+    }
+
+    public void StopShoot(){
+
+        //Shoot();
+        if (_currentCoroutine != null){
+            StopCoroutine(_currentCoroutine);
+        }
+    }
+
+}
