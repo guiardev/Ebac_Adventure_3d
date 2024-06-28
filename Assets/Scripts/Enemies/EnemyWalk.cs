be1818d976502c8640f4ca96bf126cbdfb8e8555
Guilherme A. R <guiabucarma@gmail.com>
2024-06-28 16:02:15 -0400

    add script EnemyWalk in Enemies

---
+++ Assets/Scripts/Enemies/EnemyWalk.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy{
+
+    public class EnemyWalk : EnemyBase{
+
+        [Header("Waypoints")]
+        private int _index = 0;
+        public GameObject[] waypoints;
+        public float minDistance = 1f, speed = 1f;
+
+        public override void Update() {
+
+            base.Update();
+
+            //fazendo inimigo andar seguindo os waypoints
+            if(Vector3.Distance(transform.position, waypoints[_index].transform.position) < minDistance){
+
+                _index++;
+
+                if(_index >= waypoints.Length){
+                    _index = 0;
+                }
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[_index].transform.position, Time.deltaTime * speed);
+            transform.LookAt(waypoints[_index].transform.position); // fazendo inimigo olhar na direcao do waypoints
+        }
+    }
+}
