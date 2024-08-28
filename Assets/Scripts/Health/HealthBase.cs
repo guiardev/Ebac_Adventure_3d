4b32a5ccc81eca0e96e70b841f286bfcda2eb6fe
Guilherme A. R <guiabucarma@gmail.com>
2024-08-28 15:54:36 -0400

    Update variable damageMultiply, method ChangeDamageMultiply and IEnumerator ChangeDamageMultiplyCoroutine

--- Assets/Scripts/Health/HealthBase.cs
+++ Assets/Scripts/Health/HealthBase.cs
@@ -2,13 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cloth;
+
+
 
 public class HealthBase : MonoBehaviour, IDamageable{
 
     [SerializeField] private float _currentLife;
     public Action<HealthBase> OnDamage, OnKill;
     public List<UIFillUpdater> uiGunUpdater;
-    public float startLife = 10f;
+    public float startLife = 10f, damageMultiply = 1;
     public bool destroyOnKill = false;
 
     private void Awake(){
@@ -42,7 +45,7 @@ public class HealthBase : MonoBehaviour, IDamageable{
 
         //transform.position -= transform.forward; // fazendo personagem que levou tiro afastar com impacto do tiro
 
-        _currentLife -= f;
+        _currentLife -= f * damageMultiply; // damageMultiply tendo noção exata que esta mudando.
 
         if (_currentLife <= 0){
             Kill();
@@ -62,4 +65,16 @@ public class HealthBase : MonoBehaviour, IDamageable{
             uiGunUpdater.ForEach(i => i.UpdateValue((float)_currentLife / startLife));
         }
     }
+
+    public void ChangeDamageMultiply(float damage, float duration){
+        StartCoroutine(ChangeDamageMultiplyCoroutine(damageMultiply, duration));
+    }
+
+    IEnumerator ChangeDamageMultiplyCoroutine(float damageMultiply, float duration){
+ 
+        this.damageMultiply = damageMultiply; // this esta mostrando variável da damageMultiply class HealthBase
+        yield return new WaitForSeconds(duration);
+        this.damageMultiply = 1;
+    }
+
 }
